abee0eb0ac66ac1cc98c01a624adcc6dd8e9f30b
Gustavo Alexandre da Silva Carvalho <gustavoalexandrescarvalho@gmail.com>
2022-04-15 11:21:48 -0300

    Consumir serviço de autenticação

--- gatitobook/src/app/home/login/login.component.ts
+++ gatitobook/src/app/home/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  usuario = '';
+  senha = '';
 
-  constructor() { }
+  constructor(private authService: AutenticacaoService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      (res) => {
+        console.log('Login efetuado com sucesso');
+        console.log(res);
+      },
+      (err) => {
+        console.log('Erro ao efetuar login');
+        console.log(err);
+      }
+    );
   }
-
 }
