2002af9587ace3d9a30aab5ce7e0a6bd88cb711e
Gustavo Alexandre da Silva Carvalho <gustavoalexandrescarvalho@gmail.com>
2022-05-02 19:12:27 -0300

    Adicionando comentario

--- gatitobook/src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
+++ gatitobook/src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { ComentariosService } from './comentarios.service';
+import { Observable } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
+import { Comentarios } from './comentarios';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-comentarios',
   templateUrl: './comentarios.component.html',
-  styleUrls: ['./comentarios.component.css']
+  styleUrls: ['./comentarios.component.css'],
 })
 export class ComentariosComponent implements OnInit {
+  @Input() id!: number;
+  comentarios$!: Observable<Comentarios>;
+  comentarioForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private comentarioService: ComentariosService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.comentarios$ = this.comentarioService.buscaComentario(this.id);
+
+    this.comentarioForm = this.formBuilder.group({
+      comentario: ['', Validators.maxLength(300)],
+    });
   }
 
+  gravar() {}
 }
